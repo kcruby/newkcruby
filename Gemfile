86767f86c32bf1e023ee5999eb05bf22794f5c12
Darren Cauthon <darren@cauthon.com>
2014-06-18 08:59:40 -0500

    Add kilt.

--- Gemfile
+++ Gemfile
@@ -41,3 +41,4 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+gem 'kilt-cms', git: 'git@github.com:AsheAvenue/Kilt.git'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+	mount Kilt::Engine => '/admin', as: 'kilt_engine'
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
