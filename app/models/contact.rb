1d7ec0771f895be7cff2ab659fc48d72035bc384
David Ames <davidames1983@gmail.com>
2014-07-30 22:50:02 -0500

    Remove random ellipsis from below asterick in contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,15 @@
 class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject
   attribute :message
-  attribute :nickname,  :captcha  => true
+  
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
+      :subject => %("#{subject}"),
       :to => "davidames1983@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
