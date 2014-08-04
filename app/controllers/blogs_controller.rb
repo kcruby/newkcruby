9e57ea79fa94d2fe34d31f1218fa1c4776d103c0
David Ames <davidames1983@gmail.com>
2014-08-04 17:34:06 -0500

    Add sample blogs. Change pagination limit to 5 per page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.paginate(:page => params[:page], :per_page => 10)
+    @blogs = Blog.paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /blogs/1
