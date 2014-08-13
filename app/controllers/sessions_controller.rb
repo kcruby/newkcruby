ce930bb286973835e2157b7240d75d457206044f
Darren Cauthon <darren@cauthon.com>
2014-08-12 22:46:41 -0500

    Spacing.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,22 @@
 class SessionsController < ApplicationController
   def new
     if signed_in?
-        redirect_to root_path
+      redirect_to root_path
     end
   end
 
   def create
-     if signed_in?
-        redirect_to root_path
-     end
-     user = User.find_by(email: params[:session][:email].downcase)
-  	 if user && user.authenticate(params[:session][:password])
-  	   sign_in user
-       redirect_back_or user
-     else
-       flash[:notice] = "Invalid email/password combination"
-       redirect_to new_session_path
-  	 end
+    if signed_in?
+      redirect_to root_path
+    end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash[:notice] = "Invalid email/password combination"
+      redirect_to new_session_path
+    end
   end
 
   def destroy
