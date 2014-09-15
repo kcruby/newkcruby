c0c9e1abd00f949fbfa3e8e6a7e7b0086c5a5d1e
Darren Cauthon <darren@cauthon.com>
2014-09-14 22:37:03 -0500

    Got the web calls under test.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,28 +4,28 @@ class WelcomeController < ApplicationController
 
   def index
   	@meetup = JSON.parse(meetup)
-    #@meetup_members = JSON.parse(meetup_members)
-    #@meetup_past_total = JSON.parse(meetup_past_total)
+    @meetup_members = JSON.parse(meetup_members)
+    @meetup_past_total = JSON.parse(meetup_past_total)
   end
 
 
-  #def meetup_members
-    #url = URI.parse('http://api.meetup.com/2/groups?group_id=575011&radius=25.0&order=id&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=2c3394f2573d80a5db423cd17683052bb35ba9fb')
-    #req = Net::HTTP::Get.new(url.to_s)
-    #res = Net::HTTP.start(url.host, url.port) {|http|
-      #http.request(req)
-    #}
-    #return res.body
-  #end
+  def meetup_members
+    url = URI.parse('http://api.meetup.com/2/groups?group_id=575011&radius=25.0&order=id&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=2c3394f2573d80a5db423cd17683052bb35ba9fb')
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    return res.body
+  end
 
-  #def meetup_past_total
-    #url = URI.parse('http://api.meetup.com/2/events?group_id=575011&status=past&order=time&limited_events=False&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=66cf2eab7cf84735622c5fdd774120f0ef20fd0b')
-    #req = Net::HTTP::Get.new(url.to_s)
-    #res = Net::HTTP.start(url.host, url.port) {|http|
-      #http.request(req)
-    #}
-    #return res.body
-  #end
+  def meetup_past_total
+    url = URI.parse('http://api.meetup.com/2/events?group_id=575011&status=past&order=time&limited_events=False&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=66cf2eab7cf84735622c5fdd774120f0ef20fd0b')
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    return res.body
+  end
 
 
   def meetup
--- spec/app/controllers/welcome_controller_spec.rb
+++ spec/app/controllers/welcome_controller_spec.rb
@@ -6,6 +6,15 @@ describe WelcomeController do
 
   describe "the index action" do
 
+    before do
+      stub_request(:any, 'http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=kcruby&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=149131642&sig=b0c1431c098b9665c479451539a165368a9fd796')
+        .to_return(body: {}.to_json)
+      stub_request(:any, 'http://api.meetup.com/2/groups?group_id=575011&radius=25.0&order=id&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=2c3394f2573d80a5db423cd17683052bb35ba9fb')
+        .to_return(body: {}.to_json)
+      stub_request(:any, 'http://api.meetup.com/2/events?group_id=575011&status=past&order=time&limited_events=False&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=66cf2eab7cf84735622c5fdd774120f0ef20fd0b')
+        .to_return(body: {}.to_json)
+    end
+
     [
       { 'a' => 'b' }, 
       { 'c' => 'd' }, 
@@ -21,6 +30,37 @@ describe WelcomeController do
           controller.index
 
           controller.instance_eval { @meetup }.must_equal example
+            
+        end
+
+      end
+
+      describe "multiple examples" do
+
+        it "should pass the meetup member data to the view" do
+
+          stub_request(:any, 'http://api.meetup.com/2/groups?group_id=575011&radius=25.0&order=id&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=2c3394f2573d80a5db423cd17683052bb35ba9fb').
+            to_return(:body => example.to_json)
+
+          controller.index
+
+          controller.instance_eval { @meetup_members }.must_equal example
+            
+        end
+
+      end
+
+
+      describe "multiple examples" do
+
+        it "should pass the meetup member data to the view" do
+
+          stub_request(:any, 'http://api.meetup.com/2/events?group_id=575011&status=past&order=time&limited_events=False&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=66cf2eab7cf84735622c5fdd774120f0ef20fd0b').
+            to_return(:body => example.to_json)
+
+          controller.index
+
+          controller.instance_eval { @meetup_past_total }.must_equal example
             
         end
 
