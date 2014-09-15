e7c01b8e68915eb8ddd6391e0cd8649a0695e164
Darren Cauthon <darren@cauthon.com>
2014-09-14 22:44:16 -0500

    Dry up the code.

--- app/models/meetup_client.rb
+++ app/models/meetup_client.rb
@@ -1,25 +1,19 @@
 module MeetupClient
 
   def self.members
-    url = URI.parse('http://api.meetup.com/2/groups?group_id=575011&radius=25.0&order=id&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=2c3394f2573d80a5db423cd17683052bb35ba9fb')
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    return JSON.parse(res.body)
+    make_web_request 'http://api.meetup.com/2/groups?group_id=575011&radius=25.0&order=id&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=2c3394f2573d80a5db423cd17683052bb35ba9fb'
   end
 
   def self.calendar
-  	url = URI.parse('http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=kcruby&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=149131642&sig=b0c1431c098b9665c479451539a165368a9fd796')
-  	req = Net::HTTP::Get.new(url.to_s)
-  	res = Net::HTTP.start(url.host, url.port) {|http|
-  	  http.request(req)
-  	}
-    return JSON.parse(res.body)
+    make_web_request 'http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=kcruby&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=149131642&sig=b0c1431c098b9665c479451539a165368a9fd796'
   end
 
   def self.past_total
-    url = URI.parse('http://api.meetup.com/2/events?group_id=575011&status=past&order=time&limited_events=False&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=66cf2eab7cf84735622c5fdd774120f0ef20fd0b')
+    make_web_request 'http://api.meetup.com/2/events?group_id=575011&status=past&order=time&limited_events=False&desc=false&offset=0&photo-host=public&format=json&page=500&fields=&sig_id=149131642&sig=66cf2eab7cf84735622c5fdd774120f0ef20fd0b'
+  end
+
+  def self.make_web_request url
+    url = URI.parse(url)
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
