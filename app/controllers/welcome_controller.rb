f2aca1059c837e188d371c7c5732e888ba8b66b2
Darren Cauthon <darren@cauthon.com>
2014-09-14 23:17:19 -0500

    Looks like a tab slipped in there.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-  	@meetup            = MeetupClient.calendar
+    @meetup            = MeetupClient.calendar
     @meetup_members    = MeetupClient.members
     @meetup_past_total = MeetupClient.past_total
   end
