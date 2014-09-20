4dec2a7beed6d1d01f68102cb8143c1d8bc3dacc
David Ames <davidames1983@gmail.com>
2014-09-20 14:43:30 -0500

    revert to HEAD

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
 
+
   def index
-    @meetup            = MeetupClient.calendar
-    @meetup_members    = MeetupClient.members
-    @meetup_past_total = MeetupClient.past_total
+      @meetup            = MeetupClient.calendar
+      @meetup_members    = MeetupClient.members
+      @meetup_past_total = MeetupClient.past_total
   end
+
 
 end
