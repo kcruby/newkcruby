6b83c0f3ae8bdf1c1507c0536660c83420620686
David Ames <davidames1983@gmail.com>
2014-09-21 20:29:47 -0500

    Add basic Rspec test for blog model

--- spec/app/models/blog_spec.rb
+++ spec/app/models/blog_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
+
 RSpec.describe Blog, :type => :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "can be instantiated" do 
+    	blog = Blog.new
+    	expect(blog).to be_a Blog
+    end
+
+    it 'can be assigned a title' do
+    	blog = Blog.new
+    	blog.title = "Blog Title"
+    	expect(blog.title).to eq("Blog Title")
+    end
+
+    it 'can be assigned a message' do
+    	blog = Blog.new
+    	blog.message = "Blog Message"
+    	expect(blog.message).to eq("Blog Message")
+    end
 end
+
+
+
+
