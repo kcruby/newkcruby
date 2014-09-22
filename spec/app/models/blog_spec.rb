51ba1534bf8157da8dad9dee72ddfd406cc099b0
David Ames <davidames1983@gmail.com>
2014-09-22 12:15:36 -0500

    Correct indention

--- spec/app/models/blog_spec.rb
+++ spec/app/models/blog_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 
 RSpec.describe Blog, :type => :model do
-    it "can be instantiated" do 
-    	blog = Blog.new
-    	expect(blog).to be_a Blog
-    end
+  it "can be instantiated" do 
+  	blog = Blog.new
+  	expect(blog).to be_a Blog
+  end
 
-    it 'can be assigned a title' do
-    	blog = Blog.new
-    	blog.title = "Blog Title"
-    	expect(blog.title).to eq("Blog Title")
-    end
+  it 'can be assigned a title' do
+  	blog = Blog.new
+  	blog.title = "Blog Title"
+  	expect(blog.title).to eq("Blog Title")
+  end
 
-    it 'can be assigned a message' do
-    	blog = Blog.new
-    	blog.message = "Blog Message"
-    	expect(blog.message).to eq("Blog Message")
-    end
+  it 'can be assigned a message' do
+  	blog = Blog.new
+  	blog.message = "Blog Message"
+  	expect(blog.message).to eq("Blog Message")
+  end
 end
 
 
