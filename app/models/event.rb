d38b5eb8ad76bd4dd8dc6ee04c840f6a7889249e
Darren Cauthon <darren@cauthon.com>
2014-11-21 09:14:26 -0600

    Moving more stuff to the model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,43 @@ class Event < HashThing
 
   def event_date
     Time.at(time/1000.0)
+  rescue
+    nil
+  end
+
+  def formatted_month
+    event_date.strftime("%B")
+  rescue
+    nil
+  end
+
+  def formatted_year
+    event_date.strftime("%Y")
+  rescue
+    nil
+  end
+
+  def formatted_day_as_number
+    event_date.strftime("#{event_date.day.ordinalize}")
+  rescue
+    nil
+  end
+
+  def formatted_day_as_string
+    event_date.strftime("%A")
+  end
+
+  def formatted_begin_hour
+     event_date.strftime("%-I")
+  rescue
+    nil
+  end
+
+  def formatted_end_hour
+    dur = Time.at((time/1000)+(duration/1000))
+    dur.strftime("%-I")
+  rescue
+    nil
   end
 
   def self.all
