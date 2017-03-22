3317f2561c42410cb4c7542b9fd3b9e827c4f4eb
Ross Fuhrman <rossfuhrman@gmail.com>
2017-03-21 20:15:16 -0500

    At least it's not broken anymore
    The site is broken-down, but at least it's not completely broken.
    The meetup API calls are not working as expected and this is a
    workaround for that.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
 
   def index
-    @meetup_members    = MeetupClient.members
-    @meetup_past_total = MeetupClient.past_total
-    @events = Event.all
+   #@meetup_members    = MeetupClient.members
+    @meetup_members_results = MeetupClient.members['results'] || []
+   #@meetup_past_total = MeetupClient.past_total
+    @meetup_past_total_results = MeetupClient.past_total['results'] || []
+   #@events = Event.all
+   @events = Event.all rescue []
   end
 
 end
