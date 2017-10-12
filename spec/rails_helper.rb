92cbc85095f11e2b632dbb68d4505dc26283a0bd
Alisher Sadikov <alishersadikov@gmail.com>
2017-10-12 09:33:12 -0500

    Whitelist api.meetup.com for web mock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'webmock/rspec'
 
+WebMock.disable_net_connect!(allow: 'api.meetup.com')
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
