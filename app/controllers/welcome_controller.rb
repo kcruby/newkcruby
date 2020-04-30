1689add6e6a76b24a45fbbb7eb3bc82f61c629ef
Ross Fuhrman <rossfuhrman@gmail.com>
2020-04-29 20:08:02 -0500

    Yolo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,9 @@ class WelcomeController < ApplicationController
 
   def index
    #@meetup_members    = MeetupClient.members
-    @meetup_members_results = MeetupClient.members['results'] || []
+    @meetup_members_results = MeetupClient.members['results'] rescue []
    #@meetup_past_total = MeetupClient.past_total
-    @meetup_past_total_results = MeetupClient.past_total['results'] || []
+    @meetup_past_total_results = MeetupClient.past_total['results'] rescue []
    #@events = Event.all
    @events = Event.all rescue []
 
